d1b125eea5962d5ad0354a6d9d38d257b6b2cfbb
ben lamothe <zonotope@gmail.com>
2015-11-28 10:52:03 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject immutant-web-component "0.1.0-SNAPSHOT"
+  :description
+  "A component for the Immutant Web Server."
+  :url "http://github.com/zonotope/immutant-web-component"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
