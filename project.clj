88035afd0b64a7d150100a7b0df95221d9f72d61
ben lamothe <zonotope@gmail.com>
2015-11-28 16:14:44 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject immutant-web-component "0.2.0-SNAPSHOT"
+(defproject immutant-web-component "0.2.0"
   :description "A component for the Immutant Web Server."
   :url "http://github.com/zonotope/immutant-web-component"
   :license {:name "The MIT License"
