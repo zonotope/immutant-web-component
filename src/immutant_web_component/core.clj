e5cb62896801a54fe3db0a1aaf09742b5de820d8
ben lamothe <zonotope@gmail.com>
2016-02-19 20:08:02 -0500

    cleanup

--- src/immutant_web_component/core.clj
+++ src/immutant_web_component/core.clj
@@ -3,20 +3,28 @@
             [immutant.web :as web])
   (:import java.net.ConnectException))
 
-(defrecord ImmutantWebServerComponent []
+(defrecord ImmutantWeb []
   component/Lifecycle
   (start [component]
     (if-not (:server component)
-      (let [host (or (:host component) "0.0.0.0")
-            port (Integer. (or (:port component) 8080))
+      (let [host (or (:host component)
+                     "0.0.0.0")
+            port (Integer. (or (:port component)
+                               8080))
             config {:host host :port port}
             handler (:app component)
             server (do (-> (str "Starting web server. Listening on host: %s "
                                 "and port: %d")
                            (format host port)
                            (println))
+
                        (web/run (:handler handler) config))]
-        (assoc component :server server :host host :port port))
+
+        (assoc component
+               :server server
+               :host host
+               :port port))
+
       component))
 
   (stop [component]
@@ -26,9 +34,10 @@
               (println))
           (web/stop server)
           (dissoc component :server))
+
       component)))
 
-(defn immutant-web-component [config]
+(defn immutant-web [config]
   "Creates a new immutant web server component that depends on a Ring handler
    component with key :app for the handler function."
-  (map->ImmutantWebServerComponent config))
+  (map->ImmutantWeb config))
